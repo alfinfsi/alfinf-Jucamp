8dae06e598eab015ae6e65d7ae1afc3cdca2cf49
pablomil2000 <martinlopezpablo@gmail.com>
2024-02-01 10:43:46 +0100

    Add Alfinf Jucamp module

---
+++ alfinfi-jucamp/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2024 Pablo Martín - Alfinf
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Alfinf Jucamp",
+    "summary": "Alfinf Jucamp",
+    "version": "16.0.1.0.0",
+    "development_status": "Beta",
+    "license": "AGPL-3",
+    "author": "Alfinf",
+    "website": "https://github.com/alfinfsi/",
+    "category": "Connector",
+    "depends": [
+        "sale",
+        "sale_management",
+        "contacts",
+    ],
+    "data": [
+    ],
+    "installable": True,
+}
